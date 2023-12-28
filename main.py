942db1d352f21c8e99c8a33715d70f0bfa7dd3d4
Tyujhty <fannybaquey@gmail.com>
2023-12-28 12:07:25 +0000

    feat: create function to add new bid. Creating the loop to restart the game. Add clear() function.

--- main.py
+++ main.py
@@ -1,2 +1,29 @@
 from replit import clear
+from art import logo
 #HINT: You can call clear() to clear the output in the console.
+
+print(logo)
+print("Welcome to the secret auction. \nWho's going to win the auction ?")
+
+auction =  []
+
+keep_add_auction = True
+
+def add_auction(name, bid, keep_add_auction):
+    new_auction = {'name': name, 'bid': bid}
+    auction.append(new_auction)
+
+
+while keep_add_auction != False:
+    name = input("What is your name?: ")
+    bid = input("What is your bid?: $")
+
+    add_auction(name, bid, keep_add_auction)
+
+    other_bidder = input("Are there any other bidders? Type 'Yes' or 'No'").lower()
+    
+    if other_bidder == 'no':
+        keep_add_auction = False
+
+    clear()
+print(auction)
